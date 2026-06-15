618568e9f1dfd82f7d914a5de07e8da29cb9f5cb
Hiro <izuti70@gmail.com>
2026-06-15 21:08:13 +0900

    new code

--- mundo 1/Desafio 4 ep 6.py
+++ mundo 1/Desafio 4 ep 6.py
@@ -1,4 +1,4 @@
 n1 = int(input("Digite um Valor: "))
 n2 = int(input("Digite outro: "))
 x = n1 + n2
-print("A soma entre {0} e {1} vale {2}" .format(n1, n2, x))
+print(f'A soma entre {n1} e {n2} vale {x} ')
