8bddc54c1979e5482aeab82c7a198f492e737bc5
Hiro <izuti70@gmail.com>
2026-06-17 15:02:37 +0900

    Fix

--- mundo 1/expy023.py
+++ mundo 1/expy023.py
@@ -1,9 +1,6 @@
 numb = int(input('digite um numero entre 0 e 9999:'))
-ns = str(numb)
-sprs = ' '.join(ns)
-divd = sprs.split()
-m = divd[0]
-c = divd[1]
-d = divd[2]
-u = divd[3]
+m = numb // 1000 % 10
+c = numb // 100 % 10
+d = numb // 10 % 10
+u = numb // 1 % 10
 print(f'Milhar:{m} \nCentena:{c} \nDezena:{d} \nUnidade:{u}')
